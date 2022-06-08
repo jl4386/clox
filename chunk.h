eca8ccb484c8524facf080ff99df6b75b8b0009c
giddens9527 <jl4386@outlook.com>
2022-06-08 16:49:44 +0800

    basic setup for chunk

---
+++ chunk.h
@@ -0,0 +1,14 @@
+//
+// Created by Jiaqi Liu on 2022/6/8.
+//
+
+#ifndef CLOX_CHUNK_H
+#define CLOX_CHUNK_H
+
+#include "common.h"
+
+typedef enum {
+    OP_RETURN,
+} OpCode;
+
+#endif //CLOX_CHUNK_H

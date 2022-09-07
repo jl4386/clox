1c675bb190f623a242c1fbdeaa6070a8caa001a0
giddens9527 <jl4386@outlook.com>
2022-09-07 18:12:29 +0800

    Basic VM binary operation

--- main.c
+++ main.c
@@ -3,7 +3,7 @@
 #include "debug.h"
 #include "vm.h"
 
-int main(int argc, const char* argv[]) {
+int main(int argc, const char *argv[]) {
     initVM();
     Chunk chunk;
     initChunk(&chunk);
@@ -12,9 +12,22 @@ int main(int argc, const char* argv[]) {
     writeChunk(&chunk, OP_CONSTANT, 123);
     writeChunk(&chunk, constant, 123);
 
+    constant = addConstant(&chunk, 3.4);
+    writeChunk(&chunk, OP_CONSTANT, 123);
+    writeChunk(&chunk, constant, 123);
+
+    writeChunk(&chunk, OP_ADD, 123);
+
+    constant = addConstant(&chunk, 5.6);
+    writeChunk(&chunk, OP_CONSTANT, 123);
+    writeChunk(&chunk, constant, 123);
+
+    writeChunk(&chunk, OP_DIVIDE, 123);
+
+    writeChunk(&chunk, OP_NEGATE, 123);
     writeChunk(&chunk, OP_RETURN, 123);
+
     interpret(&chunk);
-    disassembleChunk(&chunk, "test chunk");
     freeVM();
     freeChunk(&chunk);
     return 0;
--- vm.c
+++ vm.c
@@ -29,11 +29,18 @@ Value pop() {
 static InterpretResult run() {
 #define READ_BYTE() (*vm.ip++)
 #define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
+//multi-statements macro trick -> can not only contain multiple statements inside a block, but also permit a semicolon at the end
+#define BINARY_OP(op) \
+    do { \
+      double b = pop(); \
+      double a = pop(); \
+      push(a op b); \
+    } while (false)
 
     for (;;) {
 #ifdef DEBUG_TRACE_EXECUTION
-        printf("        ");
-        for (Value* slot = vm.stack; slot < vm.stackTop; slot ++) {
+        printf("          ");
+        for (Value *slot = vm.stack; slot < vm.stackTop; slot++) {
             printf("[ ");
             printValue(*slot);
             printf(" ]");
@@ -47,6 +54,21 @@ static InterpretResult run() {
                 printValue(pop());
                 printf("\n");
                 return INTERPRET_OK;
+            case OP_ADD:
+                BINARY_OP(+);
+                break;
+            case OP_SUBTRACT:
+                BINARY_OP(-);
+                break;
+            case OP_MULTIPLY:
+                BINARY_OP(*);
+                break;
+            case OP_DIVIDE:
+                BINARY_OP(/);
+                break;
+            case OP_NEGATE:
+                push(-pop());
+                break;
             case OP_CONSTANT: {
                 Value constant = READ_CONSTANT();
                 push(constant);
@@ -56,6 +78,7 @@ static InterpretResult run() {
     }
 #undef READ_BYTE
 #undef READ_CONSTANT
+#undef BINARY_OP
 }
 
 InterpretResult interpret(Chunk *chunk) {

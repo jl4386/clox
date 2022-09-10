760e5e59a75fb20146d3f1e856559cfda5df6d15
giddens9527 <jl4386@outlook.com>
2022-09-10 17:44:54 +0800

    Basic VM scanner setup

---
+++ compiler.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+#include "compiler.h"
+#include "common.h"
+#include "scanner.h"
+
+void compile(const char* source) {
+    initScanner(source);
+}
+
---
+++ scanner.h
@@ -0,0 +1,6 @@
+#ifndef CLOX_SCANNER_H
+#define CLOX_SCANNER_H
+
+void initScanner(const char* source);
+
+#endif //CLOX_SCANNER_H

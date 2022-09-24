69cdcb91221e0183d0bde6be3aa941f99b2398ff
giddens9527 <jl4386@outlook.com>
2022-09-24 11:39:21 +0800

    Basic Compiler parser prefix parsing

--- compiler.c
+++ compiler.c
@@ -12,6 +12,20 @@ typedef struct {
     bool panicMode;
 } Parser;
 
+typedef enum {
+    PREC_NONE,
+    PREC_ASSIGNMENT,  // =
+    PREC_OR,          // or
+    PREC_AND,         // and
+    PREC_EQUALITY,    // == !=
+    PREC_COMPARISON,  // < > <= >=
+    PREC_TERM,        // + -
+    PREC_FACTOR,      // * /
+    PREC_UNARY,       // ! -
+    PREC_CALL,        // . ()
+    PREC_PRIMARY
+} Precedence;
+
 Parser parser;
 Chunk *compilingChunk;
 
@@ -93,13 +107,36 @@ static void endCompiler() {
     emitReturn();
 }
 
+static void grouping() {
+    expression();
+    consume(TOKEN_RIGHT_PAREN, "Expect ')' after expression.");
+}
+
 static void number() {
     double value = strtod(parser.previous.start, NULL);
     emitConstant(value);
 }
 
-static void expression() {
+static void unary() {
+    TokenType operatorType = parser.previous.type;
 
+    parsePrecedence(PREC_UNARY);
+
+    switch (operatorType) {
+        case TOKEN_MINUS:
+            emitByte(OP_NEGATE);
+            break;
+        default:
+            return;
+    }
+}
+
+static void parsePrecedence(Precedence precedence) {
+
+}
+
+static void expression() {
+    parsePrecedence(PREC_ASSIGNMENT);
 }
 
 bool compile(const char *source, Chunk *chunk) {

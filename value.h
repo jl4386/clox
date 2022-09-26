2320a9a5225259bc25e70abaa16751cd00d087b5
giddens9527 <jl4386@outlook.com>
2022-09-26 11:45:56 +0800

    Basic Compiler parser tagged union

--- value.h
+++ value.h
@@ -3,17 +3,32 @@
 
 #include "common.h"
 
-typedef double Value;
+typedef enum {
+    VAL_BOOL,
+    VAL_NIL,
+    VAL_NUMBER,
+} ValueType;
+
+typedef struct {
+    ValueType type;
+    union {
+        bool boolean;
+        double number;
+    } as;
+} Value;
 
 typedef struct {
     int capacity;
     int count;
-    Value* values;
+    Value *values;
 } ValueArray;
 
-void initValueArray(ValueArray* array);
-void writeValueArray(ValueArray* array, Value value);
-void freeValueArray(ValueArray* array);
+void initValueArray(ValueArray *array);
+
+void writeValueArray(ValueArray *array, Value value);
+
+void freeValueArray(ValueArray *array);
+
 void printValue(Value value);
 
 #endif //CLOX_VALUE_H

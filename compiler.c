67948c4a3de8d5dbdd9af9be4f4cfc08663648ca
giddens9527 <jl4386@outlook.com>
2022-09-28 11:51:45 +0800

    Basic Compiler parser logical operators

--- compiler.c
+++ compiler.c
@@ -6,7 +6,9 @@
 #include "scanner.h"
 
 #ifdef DEBUG_PRINT_CODE
+
 #include "debug.h"
+
 #endif
 
 typedef struct {
@@ -118,7 +120,7 @@ static void emitConstant(Value value) {
 static void endCompiler() {
     emitReturn();
 #ifdef DEBUG_PRINT_CODE
-    if(!parser.hadError) {
+    if (!parser.hadError) {
         disassembleChunk(currentChunk(), "code");
     }
 #endif
@@ -148,8 +150,42 @@ static void binary() {
         case TOKEN_SLASH:
             emitByte(OP_DIVIDE);
             break;
+        case TOKEN_BANG_EQUAL:
+            emitBytes(OP_EQUAL, OP_NOT);
+            break;
+        case TOKEN_EQUAL_EQUAL:
+            emitByte(OP_EQUAL);
+            break;
+        case TOKEN_GREATER:
+            emitByte(OP_GREATER);
+            break;
+        case TOKEN_GREATER_EQUAL:
+            emitBytes(OP_LESS, OP_NOT);
+            break;
+        case TOKEN_LESS:
+            emitByte(OP_LESS);
+            break;
+        case TOKEN_LESS_EQUAL:
+            emitBytes(OP_GREATER, OP_NOT);
+            break;
         default:
             return; // Unreachable.
+    }
+}
+
+static void literal() {
+    switch (parser.previous.type) {
+        case TOKEN_NIL:
+            emitByte(OP_NIL);
+            break;
+        case TOKEN_TRUE:
+            emitByte(OP_TRUE);
+            break;
+        case TOKEN_FALSE:
+            emitByte(OP_FALSE);
+            break;
+        default:
+            return;
     }
 }
 
@@ -160,7 +196,7 @@ static void grouping() {
 
 static void number() {
     double value = strtod(parser.previous.start, NULL);
-    emitConstant(value);
+    emitConstant(NUMBER_VAL(value));
 }
 
 static void unary() {
@@ -171,6 +207,9 @@ static void unary() {
     switch (operatorType) {
         case TOKEN_MINUS:
             emitByte(OP_NEGATE);
+            break;
+        case TOKEN_BANG:
+            emitByte(OP_NOT);
             break;
         default:
             return;
@@ -189,31 +228,31 @@ ParseRule rules[] = {
         [TOKEN_SEMICOLON]     = {NULL, NULL, PREC_NONE},
         [TOKEN_SLASH]         = {NULL, binary, PREC_FACTOR},
         [TOKEN_STAR]          = {NULL, binary, PREC_FACTOR},
-        [TOKEN_BANG]          = {NULL, NULL, PREC_NONE},
-        [TOKEN_BANG_EQUAL]    = {NULL, NULL, PREC_NONE},
+        [TOKEN_BANG]          = {unary, NULL, PREC_NONE},
+        [TOKEN_BANG_EQUAL]    = {NULL, binary, PREC_EQUALITY},
         [TOKEN_EQUAL]         = {NULL, NULL, PREC_NONE},
-        [TOKEN_EQUAL_EQUAL]   = {NULL, NULL, PREC_NONE},
-        [TOKEN_GREATER]       = {NULL, NULL, PREC_NONE},
-        [TOKEN_GREATER_EQUAL] = {NULL, NULL, PREC_NONE},
-        [TOKEN_LESS]          = {NULL, NULL, PREC_NONE},
-        [TOKEN_LESS_EQUAL]    = {NULL, NULL, PREC_NONE},
+        [TOKEN_EQUAL_EQUAL]   = {NULL, binary, PREC_EQUALITY},
+        [TOKEN_GREATER]       = {NULL, binary, PREC_COMPARISON},
+        [TOKEN_GREATER_EQUAL] = {NULL, binary, PREC_COMPARISON},
+        [TOKEN_LESS]          = {NULL, binary, PREC_COMPARISON},
+        [TOKEN_LESS_EQUAL]    = {NULL, binary, PREC_COMPARISON},
         [TOKEN_IDENTIFIER]    = {NULL, NULL, PREC_NONE},
         [TOKEN_STRING]        = {NULL, NULL, PREC_NONE},
         [TOKEN_NUMBER]        = {number, NULL, PREC_NONE},
         [TOKEN_AND]           = {NULL, NULL, PREC_NONE},
         [TOKEN_CLASS]         = {NULL, NULL, PREC_NONE},
         [TOKEN_ELSE]          = {NULL, NULL, PREC_NONE},
-        [TOKEN_FALSE]         = {NULL, NULL, PREC_NONE},
+        [TOKEN_FALSE]         = {literal, NULL, PREC_NONE},
         [TOKEN_FOR]           = {NULL, NULL, PREC_NONE},
         [TOKEN_FUN]           = {NULL, NULL, PREC_NONE},
         [TOKEN_IF]            = {NULL, NULL, PREC_NONE},
-        [TOKEN_NIL]           = {NULL, NULL, PREC_NONE},
+        [TOKEN_NIL]           = {literal, NULL, PREC_NONE},
         [TOKEN_OR]            = {NULL, NULL, PREC_NONE},
         [TOKEN_PRINT]         = {NULL, NULL, PREC_NONE},
         [TOKEN_RETURN]        = {NULL, NULL, PREC_NONE},
         [TOKEN_SUPER]         = {NULL, NULL, PREC_NONE},
         [TOKEN_THIS]          = {NULL, NULL, PREC_NONE},
-        [TOKEN_TRUE]          = {NULL, NULL, PREC_NONE},
+        [TOKEN_TRUE]          = {literal, NULL, PREC_NONE},
         [TOKEN_VAR]           = {NULL, NULL, PREC_NONE},
         [TOKEN_WHILE]         = {NULL, NULL, PREC_NONE},
         [TOKEN_ERROR]         = {NULL, NULL, PREC_NONE},
@@ -230,7 +269,7 @@ static void parsePrecedence(Precedence precedence) {
     prefixRule();
     while (precedence <= getRule(parser.current.type)->precedence) {
         advance();
-        ParseFn infixRule = getRule(parser.previous.type) -> infix;
+        ParseFn infixRule = getRule(parser.previous.type)->infix;
         infixRule();
     }
 }
--- value.c
+++ value.c
@@ -9,7 +9,7 @@ void initValueArray(ValueArray *array) {
     array->values = NULL;
 }
 
-void freeValueArray(ValueArray* array) {
+void freeValueArray(ValueArray *array) {
     FREE_ARRAY(Value, array->values, array->capacity);
     initValueArray(array);
 }
@@ -25,5 +25,29 @@ void writeValueArray(ValueArray *array, Value value) {
 }
 
 void printValue(Value value) {
-    printf("%g", value);
+    switch (value.type) {
+        case VAL_BOOL:
+            printf(AS_BOOL(value) ? "true" : "false");
+            break;
+        case VAL_NIL:
+            printf("nil");
+            break;
+        case VAL_NUMBER:
+            printf("%g", AS_NUMBER(value));
+            break;
+    }
+}
+
+bool valuesEqual(Value a, Value b) {
+    if (a.type != b.type) return false;
+    switch (a.type) {
+        case VAL_BOOL:
+            return AS_BOOL(a) == AS_BOOL(b);
+        case VAL_NIL:
+            return true;
+        case VAL_NUMBER:
+            return AS_NUMBER(a) == AS_NUMBER(b);
+        default:
+            return false;
+    }
 }

4be47f7e2660cbf88c002fd9e8be5216062e92af
giddens9527 <jl4386@outlook.com>
2022-09-29 18:08:51 +0800

    Basic Compiler - string

--- object.c
+++ object.c
@@ -1,5 +1,40 @@
 //
 // Created by Jiaqi Liu on 2022/9/29.
 //
+#include <stdio.h>
+#include <string.h>
 
+#include "memory.h"
 #include "object.h"
+#include "value.h"
+#include "vm.h"
+
+#define ALLOCATE_OBJ(type, objectType) (type*)allocateObj(sizeof(type), objectType)
+
+Obj *allocateObj(size_t size, ObjType type) {
+    Obj *object = (Obj *) reallocate(NULL, 0, size);
+    object->type = type;
+    return object;
+}
+
+ObjString *allocateString(char *chars, int length) {
+    ObjString *string = ALLOCATE_OBJ(ObjString, OBJ_STRING);
+    string->length = length;
+    string->chars = chars;
+    return string;
+}
+
+ObjString *copyString(const char *chars, int length) {
+    char *heapChars = ALLOCATE(char, length + 1);
+    memcpy(heapChars, chars, length);
+    heapChars[length] = '\0';
+    return allocateString(heapChars, length);
+}
+
+void printObject(Value value) {
+    switch (OBJ_TYPE(value)) {
+        case OBJ_STRING:
+            printf("%s", AS_CSTRING(value));
+            break;
+    }
+}

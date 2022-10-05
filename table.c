46315e1490d40163ddfc69e630538c3ff9890516
giddens9527 <jl4386@outlook.com>
2022-10-05 21:46:39 +0800

    Basic Compiler - hash set and get

--- table.c
+++ table.c
@@ -19,6 +19,43 @@ void freeTable(Table *table) {
   initTable(table);
 }
 
+static Entry *findEntry(Entry *entries, int capacity, ObjString *key) {
+  uint32_t index = key->hash % capacity;
+  for (;;) {
+    Entry *entry = &entries[index];
+    if (entry->key == key || entry->key == NULL) {
+      return entry;
+    }
+    index = (index + 1) % capacity;
+  }
+}
+
+static void adjustCapacity(Table *table, int capacity) {
+  Entry *entries = ALLOCATE(Entry, capacity);
+  for (int i = 0; i < capacity; i++) {
+    entries[i].key = NULL;
+    entries[i].value = NIL_VAL;
+  }
+  for (int i = 0; i < table->capacity; i++) {
+    Entry *entry = &table->entries[i];
+    if (entry->key == NULL) continue;
+    Entry *dest = findEntry(entries, capacity, entry->key);
+    dest->key = entry->key;
+    dest->value = entry->value;
+  }
+  FREE_ARRAY(Entry, table->entries, table->capacity);
+  table->entries = entries;
+  table->capacity = capacity;
+}
+
+bool tableGet(Table *table, ObjString *key, Value *value) {
+  if (table->count == 0) return false;
+  Entry *entry = findEntry(table->entries, table->capacity, key);
+  if (entry->key == NULL) return false;
+  *value = entry->value;
+  return true;
+}
+
 bool tableSet(Table *table, ObjString *key, Value value) {
   if (table->count + 1 > table->capacity * TABLE_MAX_LOAD) {
     int capacity = GROW_CAPACITY(table->capacity);
@@ -31,4 +68,13 @@ bool tableSet(Table *table, ObjString *key, Value value) {
   entry->key = key;
   entry->value = value;
   return isNewKey;
+}
+
+void tableAddAll(Table *from, Table *to) {
+  for (int i = 0; i < from->capacity; i++) {
+    Entry *entry = &from->entries[i];
+    if (entry->key != NULL) {
+      tableSet(to, entry->key, entry->value);
+    }
+  }
 }
--- table.h
+++ table.h
@@ -17,6 +17,8 @@ typedef struct {
 
 void initTable(Table *table);
 void freeTable(Table *table);
+bool tableGet(Table *table, ObjString *key, Value *value);
 bool tableSet(Table *table, ObjString *key, Value value);
+void tableAddAll(Table *from, Table *to);
 
 #endif  // CLOX__TABLE_H_

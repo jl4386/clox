d6cdf1effe41d2b67d4b3238ca4ea359219fec0a
giddens9527 <jl4386@outlook.com>
2022-10-10 16:55:49 +0800

    Basic Compiler - global variable get

--- vm.c
+++ vm.c
@@ -134,6 +134,16 @@ static InterpretResult run() {
         break;
       case OP_POP: pop();
         break;
+      case OP_GET_GLOBAL: {
+        ObjString *name = READ_STRING();
+        Value value;
+        if (!tableGet(&vm.globals, name, &value)) {
+          runtimeError("Undefined variable %s.", name->chars);
+          return INTERPRET_RUNTIME_ERROR;
+        }
+        push(value);
+        break;
+      }
       case OP_DEFINE_GLOBAL: {
         ObjString *name = READ_STRING();
         tableSet(&vm.globals, name, peek(0));
